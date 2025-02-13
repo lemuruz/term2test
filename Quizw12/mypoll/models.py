5364145189fe4a5503ec68a703bd907c66ca066a
lemuruz <nakabodeepatiparn@gmail.com>
2025-02-13 15:50:35 +0700

    add poll to project urls

--- Quizw12/mypoll/models.py
+++ Quizw12/mypoll/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# class polls(models.Model):
+#     Pname = models.CharField(max_length=20, unique=True)
+#     choice = models.Choices(value=1,names=models.CharField(max_length=20, unique=True))
+
+#     def __str__(self):
+#         return self.name
+    
+
+
