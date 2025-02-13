0cf4954c7b40d7847068ccc758f588c47367a6d1
lemuruz <nakabodeepatiparn@gmail.com>
2025-02-13 16:01:11 +0700

    add database to poll app

--- Quizw12/mypoll/models.py
+++ Quizw12/mypoll/models.py
@@ -1,11 +1,14 @@
-# from django.db import models
+from django.db import models
 
-# class polls(models.Model):
-#     Pname = models.CharField(max_length=20, unique=True)
-#     choice = models.Choices(value=1,names=models.CharField(max_length=20, unique=True))
+class polls(models.Model):
+    Pname = models.CharField(max_length=20, unique=True)
+    choice = models.Choices(value=1,names=models.CharField)
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
     
 
+class choice(models.Model):
+    Cname = models.CharField(max_length=20, unique=True)
+    count = models.IntegerField()
 
--- Quizw12/mypoll/views.py
+++ Quizw12/mypoll/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,"mypoll/poll_index.html")
+    polls = None
+    return render(request,"mypoll/poll_index.html",{'polls' : polls})
